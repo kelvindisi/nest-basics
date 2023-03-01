315568002bc83067df0988b0a3e80e388fba8ce9
Kelvin Ndisi <kelvindisi@gmail.com>
2023-03-01 13:56:15 +0300

    user registration and login enabled and token is gerated upon successfull login and signing up

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,15 +2,22 @@ import {
   BadRequestException,
   Injectable,
   NotAcceptableException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { LoginDto, RegistrationDto } from './dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signUp(dto: RegistrationDto) {
     dto.password = await argon.hash(dto.password);
@@ -30,10 +37,43 @@ export class AuthService {
         throw new BadRequestException(error.message);
       });
 
-    return user;
+    return this.signToken(user.id, user.email);
   }
 
   async signIn(dto: LoginDto) {
-    return dto;
+    const user = await this.prisma.user
+      .findUnique({
+        where: {
+          email: dto.email,
+        },
+      })
+      .then(async (data) => {
+        const pwVerified = argon.verify(data.password, dto.password);
+        if (!pwVerified) {
+          throw new UnauthorizedException('wrong login credentials');
+        }
+        return data;
+      })
+      .catch((error) => {
+        throw new BadRequestException(error);
+      });
+
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: email,
+      id: userId,
+    };
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: this.config.get('TOKEN_EXPIRES_IN'),
+      secret: this.config.get('SIGNING_SECRET'),
+    });
+
+    return { access_token: token };
   }
 }
